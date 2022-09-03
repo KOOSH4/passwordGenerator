f2a77e3ba6117b85ea2d5db20f94ad3e7665fe38
koosh4 <koosha.owlad@gmail.com>
2022-09-03 12:40:03 +0200

    Removed old files and added webpack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./dist/*.html'],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
