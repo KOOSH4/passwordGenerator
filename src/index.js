bbeb484d1e417111226b07c2c7bb883bdaebe14e
koosh4 <koosha.owlad@gmail.com>
2022-09-04 17:11:50 +0200

    Add JS funtionallity to slider and char counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+let slider = document.getElementById('myRange');
+let output = document.getElementById('outputLength');
+output.innerHTML = slider.value;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
